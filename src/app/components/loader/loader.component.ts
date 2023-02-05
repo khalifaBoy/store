6a5e27b01cd70fcc2e340a8bd54c70e6adac9b92
khalifaBoy <102462964+khalifaBoy@users.noreply.github.com>
2023-02-05 23:06:16 +0530

    correction to loading component

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -5,41 +5,46 @@ import { Component } from '@angular/core';
   template: 
   `
   <style>
-  .loader 
-    height: 100px
-    width: 100px
-    display: flex
-    justify-content: space-between
-    align-items: center
-    animation: spin 3s infinite linear
-  
+  .loader {
+    height: 100px;
+    width: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    animation: spin 3s infinite linear;
+    margin: 0 auto;
+    margin-top: 10vh;
+  }
 
-  .red 
-    background: #f65314 
-    width: 30px 
-    height: 30px
-    border-radius: 50%
-  
-  .yellow 
-    background: #ffbb00 
-    width: 30px 
-    height: 30px
-    border-radius: 50%
-  
+  .red {
+    background: #f65314;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
+
+  .yellow {
+    background: #ffbb00;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
 
 
-  @keyframes spin 
-    0% 
-        transform: rotate(0deg)
-        width: 100px
-    
-    50% 
-        transform: rotate(360deg)
-        width: 68px
-    
-    100% 
-        transform: rotate(720deg)
-        width: 100px
+  @keyframes spin {
+    0% {
+        transform: rotate(0deg);
+        width: 100px;
+    }
+    50% {
+        transform: rotate(360deg);
+        width: 68px;
+    }
+    100% {
+        transform: rotate(720deg);
+        width: 100px;
+    }
+  }
   </style>
   <div class="loader">
   <div class="red"></div> <div class="yellow"></div>
